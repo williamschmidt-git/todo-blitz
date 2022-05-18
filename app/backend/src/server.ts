092c372f13db2d2d67158fb9b0f9cafa7470cb3f
William Schmidt <9williamschmidt@gmail.com>
2022-05-18 12:27:44 -0300

    new: tsconfig working properly, npm run dev script created

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,12 +1,5 @@
-import express from 'express';
-import 'dotenv/config';
-
-const app = express();
+import app from "./app";
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
-})
-
-app.listen(PORT, () => {`listen on port ${PORT}`});
+app.listen(PORT, () => {console.log({PORT})});
