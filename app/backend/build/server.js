da23194f7ca94ae91d971b41a9a5e3e2a3b0413d
William Schmidt <9williamschmidt@gmail.com>
2022-05-19 19:21:35 -0300

    trying first deploy

--- app/backend/build/server.js
+++ app/backend/build/server.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 require('dotenv').config();
-const PORT = process.env.PORT || 3002;
+const PORT = Number(process.env.PORT) || 3002;
 app_1.default.listen(PORT, () => console.warn(`listening on port: ${PORT}`));
 //# sourceMappingURL=server.js.map
