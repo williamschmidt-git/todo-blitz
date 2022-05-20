52fee3830b5783880e0d7ce1bb62554a698cd743
William Schmidt <9williamschmidt@gmail.com>
2022-05-20 11:27:32 -0300

    change readme and create npm start script at root folder

--- app/backend/build/app.js
+++ app/backend/build/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
 const error_1 = __importDefault(require("./middlewares/error"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.use('/', index_routes_1.default);
 app.use(error_1.default);
 exports.default = app;
--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import * as express from 'express';
 import router from './routes/index.routes';
 import error from './middlewares/error';
+import * as cors from 'cors';
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
-app.use('/', router);
+app.use('/todo', router);
 app.use(error);
 
 export default app;
